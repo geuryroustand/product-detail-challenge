9a80cfd92d81914da963673ba0a7415a61c8ca5f
Geury <theromantic2010@hotmail.com>
2023-12-23 09:19:51 +0100

    project setup

---
+++ backend/src/server.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+
+const server = express();
+
+server.use(
+  cors({
+    credentials: true,
+  })
+);
+
+server.use(compression());
+server.use(cookieParser());
+server.use(bodyParser.json());
+
+http.createServer(server);
+
+const port = 8080;
+
+server.listen(port, async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://geury:TSN2nqlg3MgL9XCw@cluster0.8gw6mh4.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    console.table(listEndpoints(server));
+    console.log("Server successfully running!" + port);
+  } catch (error) {
+    console.error("Db connection is failed ", error);
+  }
+});
